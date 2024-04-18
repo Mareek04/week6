011adadf087679259c4ff5d981c8d934f059e95f
Mareek04 <lakunleomolekan@gmail.com>
2024-04-18 14:31:13 +0100

    feat:first-commit

---
+++ string.js
@@ -0,0 +1,18 @@
+// String method
+
+const sentence = 'the quick brown fox jumos over the lazy dog'
+
+
+// length of a String
+console.log(sentence.length)
+
+// convert to uppercase
+document.write(sentence.toUpperCase())
+
+// convert to lowercase
+document.write(sentence.toLowerCase())
+
+// cut some part pf a string
+console.log(sentence.slice(0, 11))
+
+// finding index 
---
+++ two.js
@@ -0,0 +1,28 @@
+let num1
+let num2
+let num12 =90
+const num3 =77
+
+
+let $name
+let _name
+let _
+let $ 
+
+let firstname
+
+// function addnum3(){
+    // let num3}
+
+    num1 =80
+    num12 =800
+
+    let myname = 'francis'
+
+    let fullName = "francis enemuo"
+
+    let workplace = Univelcity
+
+   let statement =`my name is ${fullname} and I was born in ${new Date(),getFullYear() -37} and i woek for ${workPlace} amd I am a ${isMale ? 'male' : female}`
+
+   console.log(statement)
